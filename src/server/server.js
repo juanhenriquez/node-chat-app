e89f03e1d429962b8e9999d205e144fd788e5ffe
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2017-08-09 02:23:29 -0400

    add enviroment port

--- src/server/server.js
+++ src/server/server.js
@@ -2,9 +2,10 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static( path.join(__dirname, '../public') ));
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running in port ${3000}`);
 });
